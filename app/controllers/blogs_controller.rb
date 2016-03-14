babde64bdac74d35a7588182b20fc15598f1b363
HmweMya <hmwehmwe.91@gmail.com>
2016-03-14 05:53:39 +0000

    JKLM

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -47,7 +47,9 @@ class BlogsController < ApplicationController
     #@user = Blog.find(current_user)
     #@blog = user.blogs.build(blog_params)
     #@blog = Blog.new(blog_params)
-    @blog = current_user.blogs.build(blog_params)
+    #@blog = current_user.blogs.build(blog_params)
+   # @blog = Blog.new(params[:blog].permit!)
+   @blog = current_user.blogs.build(params[:blog].permit!)
     
 
     respond_to do |format|
@@ -65,7 +67,8 @@ class BlogsController < ApplicationController
   # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
-      if @blog.update(blog_params)
+      #if @blog.update(blog_params)
+      if @blog.update(params[:blog].permit!)
         format.html { redirect_to @blog, notice: 'ブログの更新に成功しました。' }
         format.json { render :show, status: :ok, location: @blog }
       else
@@ -98,9 +101,9 @@ class BlogsController < ApplicationController
       @user = current_user
     end
     # Never trust parameters from the scary internet, only allow the white list through.
-    def blog_params
-      params.require(:blog).permit(:title,:content,:user_id)
-    end
+    # def blog_params
+    #   params.require(:blog).permit(:title,:content,:user_id)
+    # end
     #def logged_in_user
       #unless logged_in?
         #store_location
