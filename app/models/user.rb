2d2fd11caa7bd0e6eceea831664a9f2fbe706b2e
HmweMya2 <hmwehmwe.91@gmail.com>
2016-03-26 08:52:33 +0000

    User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,10 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :qs
   has_many :answers
+  has_many :relationships,foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships,foreign_key: "followed_id",class_name:"Relationship", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
   devise :database_authenticatable, :registerable,#for mail confirm
          :recoverable, :rememberable, :trackable, :validatable,:confirmable, :omniauthable
   mount_uploader :image, ImageUploader       
@@ -74,5 +78,27 @@ class User < ActiveRecord::Base
   
   def self.create_unique_email 
       User.create_unique_string + "@example.com" 
+  end
+  
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+  
+  def friend 
+    User.from_users_followed_by(self)
+  end
+  
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT X.id FROM (SELECT users.* FROM users INNER JOIN relationships ON
+                          users.id=relationships.followed_id WHERE relationships.follower_id=:user_id) X INNER JOIN (SELECT users.* FROM users INNER JOIN relationships ON users.id=relationships.followed_id WHERE relationships.follower_id=:user_id) Y ON X.id=Y.id"
+                          where("id IN(#{followed_user_ids})", user_id: user.id)
   end
 end
